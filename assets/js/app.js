dd64d27923e35156df95f5d1a03c467b025f9dd0
baonk <praseetyaa@gmail.com>
2021-05-20 14:32:48 +0700

    bug fix, update ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,7 @@ $('.owl-carousel').owlCarousel( {
   });
 });
 
+//navbar
 $('#nav a').click(function () {
     $('#nav a.active').removeClass('active');
     $(this).addClass('active');
@@ -42,6 +43,7 @@ $(window).scroll(function () {
         }
     });
 });
+
 
 /**
  * Copyright 2019 Google LLC.
@@ -75,6 +77,32 @@ $(window).scroll(function () {
 // vue
 var example1 = new Vue({
   el: '#slider',
+  data: {
+    items: [
+      { text: 'http://www.smkn1mandiraja.unaux.com',
+        src: 'assets/img/1.webp'},
+      { text: 'http://www.sikos.unaux.com', 
+        src: 'assets/img/2.webp'},
+      { text: 'http://www.rania.unaux.com',
+        src: 'assets/img/3.webp'},
+      { text: 'http://www.tabungan.unaux.com', 
+        src: 'assets/img/4.webp'},
+      { text: 'http://www.mmkproject.unaux.com',
+        src: 'assets/img/5.webp'},
+      { text: '#', 
+        src: 'assets/img/6.webp'},
+      { text: 'https://bisa.co.id',
+        src: 'assets/img/7.webp'},
+      { text: 'https://campusnet.id', 
+        src: 'assets/img/8.webp'},
+      { text: 'https://demo.campusnet.id', 
+        src: 'assets/img/9.webp'}
+    ]
+  }
+})
+
+var example2 = new Vue({
+  el: '#projects',
   data: {
     items: [
       { text: 'http://www.smkn1mandiraja.unaux.com',
