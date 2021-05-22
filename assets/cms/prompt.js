024d8aa4ba1f822002eda4ebe04b0d1c03321a23
baonk <praseetyaa@gmail.com>
2021-05-23 02:32:51 +0700

    dark asset for cms

--- assets/cms/prompt.js
+++ assets/cms/prompt.js
@@ -2,19 +2,19 @@ function activateDarkMode(){
     var script = document.createElement('script');
     script.src = "https://praseetyaa.github.io/portfolio/assets/cms/dark-mode.js";
     script.type = "module";
-    document.getElementsByTagName('head')[0].appendChild(script);
+    document.getElementsByTagName('head')[0].append(script);
 
     var link_a = document.createElement('link');
     link_a.href = "https://praseetyaa.github.io/portfolio/assets/cms/light.css";
     link_a.type = "text/css";
     link_a.rel = "stylesheet";
     link_a.media = "(prefers-color-scheme: light)";
-    document.getElementsByTagName('head')[0].appendChild(link_a);
+    document.getElementsByTagName('head')[0].append(link_a);
 
     var link_b = document.createElement('link');
     link_b.href = "https://praseetyaa.github.io/portfolio/assets/cms/dark.css";
     link_b.type = "text/css";
     link_b.rel = "stylesheet";
     link_b.media = "(prefers-color-scheme: dark)";
-    document.getElementsByTagName('head')[0].appendChild(link_b);
+    document.getElementsByTagName('head')[0].append(link_b);
 }
